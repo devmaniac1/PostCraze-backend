897aaad3284e890e2aa8e70bf9be238600511990
devmaniac1 <139785309+devmaniac1@users.noreply.github.com>
2024-07-31 17:50:56 +0530

    bug fix and update readme

---
+++ README.md
@@ -0,0 +1,7 @@
+## How to run in development
+
+### Backend
+
+1. Navigate to the `backend` directory.
+2. Run `npm install` to install dependencies.
+3. Run `node index.js` to start the server.
--- controllers/postsController.js
+++ controllers/postsController.js
@@ -3,7 +3,7 @@ const Post = require("../models/postSchema");
 const getPosts = async (req, res) => {
   try {
     const posts = await Post.find();
-    res.status(200).json(posts);
+    res.json(posts);
   } catch (error) {
     res.status(404).json({ message: error.message });
   }
