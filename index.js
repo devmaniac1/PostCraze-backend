2a9874000a26ff3b4754138366d7cbbef979cfa2
Ashfaq F <mohammed.20221656@iit.ac.lk>
2024-08-01 23:48:41 +0530

    handled sensitive data into environmemnt file

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
+require('dotenv').config();
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 
 const postRoutes = require("./routes/postsRoute");
-const commentRoutes = require("./routes/commentsRoute");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -17,12 +17,10 @@ app.use(bodyParser.json());
 app.use(cors());
 
 const mongoDB =
-  "mongodb+srv://ashfaq:NPIyzdBVvdkLoMI7@postcraze.jcgy6fc.mongodb.net/?retryWrites=true&w=majority&appName=postcraze";
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@postcraze.jcgy6fc.mongodb.net/?retryWrites=true&w=majority&appName=postcraze`;
 
 app.use("/posts", postRoutes);
-app.use("/comments", commentRoutes);
 
-// console.log(postRoutes);
 mongoose
   .connect(mongoDB)
   .then(() => {
